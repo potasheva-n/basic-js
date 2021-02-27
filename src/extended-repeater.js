d22d5a53cfaf3337b2ba19ff083172a2e4751dc0
Ginko <mad.kyon@gmail.com>
2021-02-27 16:38:36 +0500

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let res='', add='';
+
+  if(options.separator === undefined) options.separator='+';
+  if(options.additionSeparator===undefined) options.additionSeparator ='';
+  if(options.addition===undefined) options.addition='';
+
+  for(let i=0; i<options.additionRepeatTimes-1; i++){   //собираем add
+    add=add+options.addition+options.additionSeparator;
+  }
+  add=add+options.addition;                             //последний add без разделителя
+
+  for(let j=0; j<options.repeatTimes-1; j++){           //собираем результирующую строку
+    res=res+str+add+options.separator;
+  }
+  return (options.repeatTimes===undefined)?str+options.addition: res+str+add; //если без повторений, то строка+add, иначе результирующая строка без разделителя
 };
-  
