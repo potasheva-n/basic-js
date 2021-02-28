7c063dfe1d82a00d6b2b314d515ed4e112a6ff78
Ginko <mad.kyon@gmail.com>
2021-02-28 09:55:46 +0500

    feat: add vigenere-cipher function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    return 0;
+
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct){
+    if(direct===undefined) direct = true;
+    this.direct = direct;
+  }
+
+  encrypt(message, key) {
+    const abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    const s = [' ', '!', ',','?','-','.','+','*',':', '$', '@', '#', '%','^','=','(',')','&','|','/',';','1','2','3','4','5','6','7','8','9','0'];
+    let m = message.toUpperCase().split(''),
+        k = key.toUpperCase().split(''),
+        result=[];
+    for(let i=0, j=0; i<m.length; i++, j++){
+
+      if(s.indexOf(m[i])!==-1){ result.push(m[i]); --j; continue;}
+
+      result.push(abc[(abc.indexOf(m[i]) + abc.indexOf(k[j]))%26]);
+      if(j+1 == k.length) j=-1;
+    }
+    return this.direct ? result.join(''):result.reverse().join('') ;
+  }
+
+  decrypt(message, key) {
+    const abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    const s = [' ', '!', ',','?','-','.','+','*',':', '$', '@', '#', '%','^','=','(',')','&','|','/',';','1','2','3','4','5','6','7','8','9','0'];
+    let m = message.toUpperCase().split(''),
+        k = key.toUpperCase().split(''),
+        result=[];
+    for(let i=0, j=0; i<m.length; i++, j++){
+
+      if(s.indexOf(m[i])!==-1){ result.push(m[i]); --j; continue;}
+
+      result.push(abc[(abc.indexOf(m[i]) + 26 - abc.indexOf(k[j]))%26]);
+      if(j+1 == k.length) j=-1;
+    }
+    return this.direct ? result.join(''):result.reverse().join('') ;
   }
 }
 
